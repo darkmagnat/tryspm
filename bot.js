f4726b66e38c061ba2fb54eb2f7f4dbc40316949
darkmagnat <43116040+darkmagnat@users.noreply.github.com>
2018-11-15 18:28:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var prefix = "1";
 
 client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('ready', async() => {
